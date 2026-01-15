bac0032bf42c8dca04a5074d1a6f3112d1c4f679
Babu-Abel Wanyeki <bwanyeki12@gmail.com>
2026-01-14 22:44:48 -0500

    Decrease SPI speed

--- main/src/bin/main.rs
+++ main/src/bin/main.rs
@@ -146,7 +146,7 @@ async fn main(spawner: Spawner) {
 
     let spi = Spi::new(
         peripherals.SPI2,
-        SPIConfig::default().with_frequency(30u32.MHz()),
+        SPIConfig::default().with_frequency(20u32.MHz()),
     )
     .unwrap()
     .with_mosi(peripherals.GPIO10)
